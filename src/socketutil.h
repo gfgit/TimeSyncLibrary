ed655eb64cfe544521f74da9903cf89d24bf4dce
LupoLucio <farinati.lucio@gmail.com>
2022-11-20 22:41:43 +0100

    commenti

--- src/socketutil.h
+++ src/socketutil.h
@@ -14,7 +14,9 @@
 
 namespace TimeSyncLibrary{
     
-    
+    /*!
+    Classe che realizza la connessione del client
+    */
     class Socket
     {
     public:
@@ -22,32 +24,37 @@ namespace TimeSyncLibrary{
         virtual ~Socket();
 
         int close();
+
+        /*! crea e connette socket all'indirizzo specificato, len indica ipv4,ipv6 (quanti byte è lungo sockaddr)*/
         int connectTo(sockaddr *serverAddr, int len);
 
+        /*! legge un buffer lungo len */
         int read(char *buf, int len);
         int write(const char *buf, int len);
 
     protected:
+        /*! descrittore del socket (id del socket)*/
         int sockFd;
     };
 
+    /*!
+    Classe che realizza un server
+    */
     class Server: public Socket
     {
     public:
         Server();
         virtual ~Server();
 
+        /*! crea socket e si mette in ascolto*/
         int listen(sockaddr *serverAddr, int len, int backLog);
     };
 
+    /*! converte l'indirizzo in byte del sockaddr in una stringa leggibile*/
     std::string IpAddressToString(sockaddr *addr, int addr_len);
 
 
 }
-
-
-
-
 
 
 
